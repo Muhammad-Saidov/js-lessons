6ed5f4aca36b8e962190610a32ea07dbcbadafa1
Zohid Saidov <zohidsaidi@gmail.com>
2025-02-19 18:25:21 +0500

    project

---
+++ 1.js
@@ -0,0 +1,1001 @@
+// function b3(){
+//     let a=5;
+// let b=6;
+// let p = 2 * (a+b);
+// console.log(p);
+// }
+
+//b4
+// let d=5;
+// let l = 3.14 *d
+// console.log(l);
+
+
+//b5
+// let a=6;
+// let v=a**3
+// let s=6*(a**2);
+// console.log(v);
+// console.log(s);
+
+
+
+//b6
+// let a=2;
+// let c=3;
+// let b=4;
+// let v=a*b*c;
+// let s=2*(a*b+b*c+a*c);
+// console.log(v);
+// console.log(s);
+
+
+
+//b7
+// let r=5;
+// let l=2*3.14*r;
+// let s=3.14*r**2
+// console.log(l);
+// console.log(s);
+
+
+
+//b8
+// let a=5;
+// let b=6;
+// let s=(a+b)/2;
+// console.log(s);
+
+
+
+// function i1(){
+//     let i=5;
+//     let m=Math.floor(i/100);
+
+//     console.log(m);
+// }
+// i1()
+
+
+
+// function i2(){
+//     let m=5;
+//     let t=Math.floor(m/1000);
+//     console.log(t);
+// }
+// i2()
+
+
+
+// function i3(){
+//     let i=5;
+//     let m=Math.floor(i/1024);
+//     console.log(m);
+// }
+// i3()
+
+
+
+// function i4(){
+//     let a=10;
+//     let b=5;
+//     let m=Math.floor(a/b);
+//     console.log(m);
+// }
+// i4()
+
+
+
+// function i5(){
+//     let a=10;
+//     let b=5;
+//     let m=Math.floor(a%b);
+//     console.log(m);
+// }
+// i5()
+
+
+
+// function i6(){
+//     let a=12;
+//     let b=Math.floor(a/10)
+//     let c=a%10
+//     console.log(b);
+//     console.log(c); 
+// }
+// i6()
+
+
+// function i7(){
+//     let a=12;
+//     let b=Math.floor(a/10)
+//     let c=a%10
+//     console.log(b*c);
+//     console.log(c+b); 
+// }
+// i7()
+
+
+// function i8(){
+//     let a=12;
+//     let b=Math.floor(a/10)
+//     let c=a%10
+//     console.log(c); 
+//     console.log(b); 
+// }
+// i8()
+
+
+
+
+
+// function i9(){
+//     let a=234;
+//     let b=Math.floor(a/100)
+//     console.log(b); 
+// }
+// i9()
+
+
+
+
+// function i10(){
+//     let a=235;
+//     let b=Math.floor(a%10)
+//     let c=Math.floor(a/10)%10
+//     console.log(b);
+//     console.log(c);
+// }
+// i10()
+
+
+
+
+// function i11(){
+//     let a=235;
+//     let b=Math.floor(a%10)
+//     let c=Math.floor(a/10)%10
+//     let d=Math.floor(a/100)
+//     let e=b*c*d;
+//     let s=b+c+d;
+//     console.log(e);
+//     console.log(s);
+// }
+// i11()
+
+
+
+
+
+// function i12(){
+//     let a=235;
+//     let b=Math.floor(a/100)
+//     let c=Math.floor(a/10)%10
+//     let d=Math.floor(a%10)
+//     console.log(d*100+c*10+b);
+// }
+// i12()
+
+
+
+
+
+// function i13(){
+//     let a=235;
+//     let b=Math.floor(a/100)
+//     let c=Math.floor(a/10)%10
+//     let d=Math.floor(a%10)
+//     console.log(d*100+b*10+c);
+// }
+// i13()
+
+
+
+
+
+// function i19(){
+//     let n=360;
+//     let minutes=Math.floor(n/60)
+//     console.log("Микдори минутхо",minutes);
+// }
+// i19()
+
+
+
+
+// function i20(){
+//     let n=3670;
+//     let hours=Math.floor(n/3600)
+//     console.log("Микдори соатхо",hours);
+// }
+// i20()
+
+
+
+// function i24(){
+//     let k=10;
+//     let n=1
+//     let c=Math.floor(n+k-1)%7
+//     console.log(c);  
+// }
+// i24()
+
+
+
+
+// function i25(){
+//     let k=2024;
+//     let n=4
+//     let c=Math.floor(n+k-1)%7
+//     console.log(c);  
+// }
+// i25()
+
+
+// function i26(){
+//     let k=2025;
+//     let n=2
+//     let c=Math.floor(n+k-1)%7
+//     console.log(c);  
+// }
+// i26()
+
+
+// function i28(){
+//     let k=25;
+//     let n=3
+//     let c=Math.floor(n+k-1)%7
+//     c=c===1?7:c
+//     console.log(c);  
+// }
+// i28()
+
+
+
+
+// function b1(){
+//     let a=5
+//     if (a>0){
+//         console.log(true);
+//     }
+//     else{
+//         console.log(false);   
+//     }
+// }
+// b1()
+
+
+
+
+
+// function b2(){
+//     let a=5
+//     if (a%2==1){
+//         console.log(true);
+//     }
+//     else{
+//         console.log(false);   
+//     }
+// }
+// b2()
+
+
+
+
+// function b3(){
+//     let a=6
+//     if (a%2==0){
+//         console.log(true);
+//     }
+//     else{
+//         console.log(false);   
+//     }
+// }
+// b3()
+
+
+
+
+
+// function b4(){
+//     let a=6
+//     if (a%2==0){
+//         console.log(true);
+//     }
+//     else{
+//         console.log(false);   
+//     }
+// }
+// b4()
+
+
+
+
+// function b4(){
+//     let a=6
+//     let b=1
+//     if (a>2 && b<=3){
+//         console.log(true);
+//     }
+//     else{
+//         console.log(false);   
+//     }
+// }
+// b4()
+
+
+
+
+// function b5(){
+//     let a=6
+//     let b=5
+//     if (a >=0 || b<-3){
+//         console.log(true);
+//     }
+//     else{
+//         console.log(false);   
+//     }
+// }
+// b5()
+
+
+
+
+
+// function b6(){
+//     let a=6
+//     let b=5
+//     let c=2
+//     if (a<b<c){
+//         console.log(true);
+//     }
+//     else{
+//         console.log(false);   
+//     }
+// }
+// b6()
+
+
+
+
+// function b7(){
+//     let a=2
+//     let b=4
+//     let c=7
+//     if ((a<b && b<c) || (c<b && b<a)){
+//         console.log(true);
+//     }
+//     else{
+//         console.log(false);   
+//     }
+// }
+// b7()
+
+
+
+
+// function b8(){
+//     let a=5
+//     let b=3
+//     if (a%2==1 && b%2==1){
+//         console.log(true);
+//     }
+//     else{
+//         console.log(false);   
+//     }
+// }
+// b8()
+
+
+
+
+// function b9(){
+//     let a=4
+//     let b=5
+//     if (a%2==1 || b%2==1){
+//         console.log(true);
+//     }
+//     else{
+//         console.log(false);   
+//     }
+// }
+// b9()
+
+
+
+// function b10(){
+//     let a=4
+//     let b=5
+//     if ((a%2==1 && b%2==0) ||(a%2==0 && b%2==1)){
+//         console.log(true);
+//     }
+//     else{
+//         console.log(false);   
+//     }
+// }
+// b10()
+
+
+
+
+
+// function b11(){
+//     let a=4
+//     let b=6
+//     if (a%2==0 && b%2==0){
+//         console.log(true);
+//     }
+//     else{
+//         console.log(false);   
+//     }
+// }
+// b11()
+
+
+
+
+// function b12(){
+//     let a=4
+//     let b=6
+//     let c=6
+//     if ((a>0)&& (b>0)&&(c>0)){
+//         console.log(true);
+//     }
+//     else{
+//         console.log(false);   
+//     }
+// }
+// b12()
+
+
+
+
+// function b13(){
+//     let a=-6
+//     let b=-6
+//     let c=-8
+//     if (a > 0 || b > 0 || c > 0){
+//         console.log(true);
+//     }
+//     else{
+//         console.log(false);   
+//     }
+// }
+// b13()
+
+
+
+
+
+// function b14(){
+//     let a=-6
+//     let b=-6
+//     let c=-8
+//     if (a > 0 || b > 0 || c > 0){
+//         console.log(true);
+//     }
+//     else{
+//         console.log(false);   
+//     }
+// }
+// b14()
+
+
+
+
+
+
+// function b15(){
+//     let a=6
+//     let b=-6
+//     let c=38
+//     if (a > 0 || b > 0 && c > 0){
+//         console.log(true);
+//     }
+//     else{
+//         console.log(false);   
+//     }
+// }
+// b15()
+
+
+
+
+
+
+// function b16(){
+//     let a=20   
+//     if (a > 9 && a < 100 && a%2==0){
+//         console.log(true);
+//     }
+//     else{
+//         console.log(false);   
+//     }
+// }
+// b16()
+
+
+
+// function b17(){
+//     let a=207
+//     if (a > 99 && a < 1000 && a%2==1){
+//         console.log(true);
+//     }
+//     else{
+//         console.log(false);   
+//     }
+// }
+// b17()
+
+
+
+
+
+
+// function b18(){
+//     let a=5
+//     let b=4
+//     let c=5
+//     if ((a==b)||(b==c)||(c==a)){
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//     }
+// }
+// b18()
+
+
+
+// function b18(){
+//     let a=5
+//     let b=4
+//     let c=5
+//     if ((a==b)||(b==c)||(c==a)){
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//     }
+// }
+// b18()
+
+
+
+
+// function b19(){
+//     let a=221
+//     if (a>99 & a<999){
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//     }
+// }
+// b19()
+
+
+
+
+// function b19(){
+//     let a=221
+//     if (a>99 & a<999){
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//      }
+// }
+//  b19()
+
+
+
+// function if1(){
+//     let a=5
+//     if (a>0){
+//         console.log(++a);
+//     }else{
+//         console.log(a);
+        
+//     }
+// }
+// if1()
+
+
+
+
+
+// function if2(){
+//     let a=-5
+//     if (a>0){
+//         console.log(++a);
+//     }
+//     else if(a<0){
+//         console.log(--a);        
+//     }
+// }
+// if2()
+
+
+
+
+
+// function if3(){
+//     let a=0
+//     if (a>0){
+//         console.log(++a);
+//     }
+//     else if(a<0){
+//         console.log(--a);        
+//     }else if(a>=0)
+//         console.log(10);
+        
+// }
+// if3()
+
+
+
+
+// function if4(){
+//     let a=-5    
+//     let b=-5
+//     let c=-8
+//     let Musbat=0
+//     let Manfi=0
+//     if (a>=0){
+//         Musbat=Musbat+1
+//     }else{
+//         Manfi=Manfi+1
+//     }
+//     if (b>=0){
+//         Musbat=Musbat+1
+//     }else{
+//         Manfi=Manfi+1
+//     }
+//     if (c>=0){
+//         Musbat=musbat+1
+//     }else{
+//         Manfi=Manfi+1
+//     }
+//     console.log("musbat",Musbat);
+//     console.log("manfi",Manfi);     
+// }
+// if4()
+
+
+
+
+// function if6(){
+//     let a=8
+//     let b=5
+//     if (a>b){
+//         console.log(a);
+//     }
+//     else if(b>a){
+//         console.log(b); 
+//     }           
+// }
+// if6()
+
+
+
+
+// function if7(){
+//     let a=9
+//     let b=11
+//     if (a<b){
+//         console.log(a);
+//     }
+//     else if(b<a){
+//         console.log(b); 
+//     }           
+// }
+// if7()
+
+
+
+
+// function if8(){
+//     let a=9
+//     let b=11
+//     if (a>b){
+//         console.log(a);
+//         console.log(b);
+        
+//     }
+//     else if(b>a){
+//         console.log(b);
+//         console.log(a); 
+//     }           
+// }
+// if8()
+
+
+
+
+// function if10(){
+//     let a=4
+//     let b=11
+//     if (a>b){
+//         console.log(a+b); 
+//     }
+//     else if(b>a){
+//         console.log(b+a);
+//     }else if(a=b)
+//         console.log(0);               
+// }
+// if10()
+
+
+
+// function if11(){
+//     let a=6
+//     let b=11
+//     if (a>b){
+//         console.log(a); 
+//     }
+//     else if(b>a){
+//         console.log(b);
+//     }else if(a=b)
+//         console.log(0);               
+// }
+// if11()
+
+
+
+
+
+// function if12(){
+//     let a=8
+//     let b=5
+//     let c=4
+//     if (a<b){
+//         console.log(a); 
+//     }else if(b<c){
+//         console.log(b);
+//     }else if(c<a){
+//         console.log(c);               
+//     }
+// }
+// if12()
+
+
+
+
+// function if13(){
+//     let a=1
+//     let b=1
+//     let c=1
+//     let mid=0
+//     if ((a>b && a<c) ||(b>a && a>c)){
+//         mid=a
+//     }
+//     if ((b>a && b<c) ||(b<a && b<c)){
+//         mid=b
+//     }
+//     if ((c>b && c<a) ||(b>c && a<c)){
+//     mid=c   
+//     }else{
+//     mid=a
+//     }
+//     console.log(mid);
+// }
+// if13()
+
+
+
+// function if14(){
+//     let a=6
+//     let b=1
+//     let c=2
+// /*     let max=Math.max(a,b,c)
+//     let min=Math.min(a,b,c) */
+//     if (a =>b ) {
+//         max=a
+//     }else{
+//         max=b
+//     }
+//     if (c>max) {
+//         max=c
+//     }
+//     if (a>b ) {
+//         min=b
+//     }else{
+//         min=a
+//     }
+//     if(min>c){
+//         min=c
+//     }
+//    console.log(max);
+//    console.log(min);
+// }
+// if14()
+
+
+
+
+// function if15(){
+//     let a=10
+//     let b=12
+//     let c=15
+//     let max=0
+//     let min=0
+//     if (a >b ) {
+//         max=a
+//     }else{
+//         max=b
+//     }
+//     if (c<max) {
+//         max=c
+//     }
+//     if (a>b ) {
+//         min=b
+//     }else{
+//         min=a
+//     }
+//     if(min<c){
+//         min=c
+//     }
+//    console.log(max);
+//    console.log(min);
+//    console.log(max+min);
+// }
+// if15()
+
+
+
+
+// function if18(){
+//     let a=18
+//     let b=17
+//     let c=18
+//     if(a==b){
+//         console.log(c);
+//     }else if(a==c){
+//         console.log(b);
+//     }else if(b==c){
+//         console.log(a);
+//     }
+// }
+// if18()
+
+
+
+// function if19(){
+//     let a=17
+//     let b=18
+//     let c=18
+//     let d=18
+//     if(a==b && b==c){
+//         console.log(d);
+//     }else if(a==c && c==d){
+//         console.log(b);
+//     }else if(b==c && c==d){
+//         console.log(a);
+//     }else if(b==d && a==d){
+//         console.log(c); 
+//     }
+// }
+// if19()
+
+
+// function if20(){
+//     let a=17
+//     let b=18
+//     let c=19
+//     let nugti_b=Math.abs(a-b)
+//     let nugti_c=Math.abs(a-c)
+//     if(nugti_b<nugti_c){
+//         console.log(`Нуктаи ${b} ба ${a} наздиктар аст. Масофа: ${nugti_b}`);
+//     } else if (nugti_c < nugti_b){
+//         console.log(`Нуктаи ${c} ба ${a} наздиктар аст. Масофа: ${nugti_c}`);
+//     } else {
+//         console.log(`Ҳардуи ${b} ва ${c} ба ${a} масофаи якхела доранд: ${nugti_b}`);
+//     }
+// }
+// if20()
+
+
+
+
+
+
+//  function case1(){
+//     let a = 3
+//     switch(a){
+//         case 1:
+//             console.log("Dushanbe");
+//             break;
+//         case 2:
+//             console.log("Seshanbe");
+//             break;
+//         case 3:
+//             console.log("Chorshanbe");
+//             break;
+//         case 4:
+//             console.log("Panjshanbe");
+//             break;
+//         case 5:
+//             console.log("Juma");
+//             break;
+//         case 6:
+//             console.log("Shanbe");
+//             break;
+//         case 7:
+//             console.log("Yakshanbe");
+//             break;
+//         default:
+//             console.log("Error");
+//             break;
+//     }
+// }
+// case1()
+
+
+
+
+
+
+// function case3(){
+//     let k = 5;
+//     switch(k){
+//         case 12:
+//         case 1:
+//         case 2:
+//             console.log("Zimiston");
+//             break;
+//         case 3:
+//         case 4:
+//         case 5:
+//             console.log("Bahor");
+//             break;
+//         case 6:
+//         case 7:
+//         case 8:
+//             console.log("Tobiston");
+//             break;
+//         case 9:
+//         case 10:
+//         case 11:
+//             console.log("Tiramoh");
+//             break;
+//         default:
+//             console.log("нодуруст");
+//             break;
+//     }
+// }
+// case3();
+
+
+
+
+
+// function for1(){
+//     let n=5;
+//     let k=3;
+//     for (let i=0; i<=n; i++){
+//         console.log(k);
+//     }
+// }
+// for1()
+
+
+// function for2(){
+//     let a=3;
+//     let b=7;
+//     let n=0
+//     for (let i=a; i<=b; i++){
+//         console.log(i);
+//         n++;
+//     }
+// }
+// for2()
+
+
+
+function for3(){
+    let a=3;
+    let b=7;
+    let n=0
+    for (let i=a; i<=b; i--){
+        console.log(i);
+        n--;
+    }
+}
+for3()
